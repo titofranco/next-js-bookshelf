e1d6efb6547dd4328d0f44ea4dc8414fbc5e03cd
Carlos Guisao Franco <carlos.guisao@globant.com>
2022-11-17 08:39:48 -0500

    Add /list page

---
+++ pages/list.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import Link from 'next/link'
+import { ListItemList } from "../components/list-item-list"
+import Layout from '../components/layout'
+
+export default function List() {
+  return (
+    <Layout>
+      <ListItemList
+        filterListItems={li => !li.finishDate}
+        noListItems={
+          <p>
+            Hey there! Welcome to your bookshelf reading list. Get started by
+            heading over to <Link href="/discover">the Discover page</Link> to add
+            books to your list.
+          </p>
+        }
+        noFilteredListItems={
+          <p>
+            Looks like you've finished all your books! Check them out in your{' '}
+            <Link href="/finished">finished books</Link> or{' '}
+            <Link href="/discover">discover more</Link>.
+          </p>
+        }
+      />
+    </Layout>
+  )
+}
